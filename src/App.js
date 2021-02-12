32e91de92a4d779cb222929ad8a206f6c33834e5
Megan Collins <meganvcollins97@gmail.com>
2021-02-12 12:37:27 +0000

    final commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { NewsContextProvider } from "./NewsContext";
 
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 import News from "./components/News.js"
+import options from "./components/Dropdown.js"
+import ReactMultiSelectCheckboxes from 'react-multiselect-checkboxes';
 
 function App() {
   return (
     <div className="widget">
       <div className="heading">
         <Header />
+        <div className="filter">
+          <ReactMultiSelectCheckboxes placeholderButtonLabel="Filter by source" className="dropdown" options={options} />
+        </div>
       </div>
       <NewsContextProvider>
         <News />
---
+++ src/components/Dropdown.js
@@ -0,0 +1,11 @@
+export const options = [
+    { label: 'CNN', value: 'CNN'},
+    { label: 'The Independent', value: 'The Independent'},
+    { label: 'The Guardian', value: 'The Guardian'},
+    { label: 'CBS News', value: 'CBS News'},
+    { label: 'New York Times', value: 'New York Times'},
+    { label: 'BBC', value: 'BBC'},
+    { label: 'Science Daily', value: 'Science Daily'},
+  ];
+
+  export default options;
